297db2c4de3f16663750bc41553d48fbef2cf952
Vadym Kravchenko <vadymkravchenko.us@gmail.com>
2026-01-12 18:52:15 -0500

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,2 +1,33 @@
-def get_human_age(cat_age: int, dog_age: int) -> list:
-    return [0, 0]
+def get_human_age(cat_age: int, dog_age: int) -> list[int]:
+    cat_human = 0
+    if cat_age > 15:
+        cat_human += 1
+        remaining = cat_age - 15
+        if remaining > 9:
+            cat_human += 1
+            remaining -= 9
+            cat_human += remaining // 4
+        else:
+            cat_human += 0
+    elif cat_age == 15:
+        cat_human = 1
+    else:
+        cat_human = 0
+
+    # Dog conversion
+    dog_human = 0
+    if dog_age > 15:
+        dog_human += 1
+        remaining = dog_age - 15
+        if remaining > 9:
+            dog_human += 1
+            remaining -= 9
+            dog_human += remaining // 5
+        else:
+            dog_human += 0
+    elif dog_age == 15:
+        dog_human = 1
+    else:
+        dog_human = 0
+
+    return [cat_human, dog_human]
