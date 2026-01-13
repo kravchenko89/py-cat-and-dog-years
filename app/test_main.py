401dcc2f504ec5cdda1397abd726fe034f3d75fc
Vadym Kravchenko <vadymkravchenko.us@gmail.com>
2026-01-12 19:14:43 -0500

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,13 +1,14 @@
 import pytest
 from app.main import get_human_age
 
+
 @pytest.mark.parametrize(
     "cat_age, dog_age, expected",
     [
         (0, 0, [0, 0]),
         (15, 15, [1, 1]),
-        (24, 24, [2, 2]),   # проверить: должно ли это быть 2 или 3 в реальной функции
-        (30, 35, [17, 18]),  # <--- исправлено
+        (24, 24, [2, 2]),
+        (30, 35, [17, 18]),
     ]
 )
 def test_human_age_conversion(cat_age: int,
